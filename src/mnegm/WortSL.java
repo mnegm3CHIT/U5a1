f06a39d9ed5c6e11bfbe1b47c83ae92c5356504f
Maged Negm <mnegm@student.tgm.at.ac>
2023-11-16 11:51:21 +0100

    speichern Methode verbessert

--- src/mnegm/WortSL.java
+++ src/mnegm/WortSL.java
@@ -4,36 +4,38 @@ import java.io.*;
 import java.nio.file.*;
 import java.util.Scanner;
 
-public class WortSL extends IOException {
-    private WortTrainer trainer;
-    public void speichern(String dataPath, String filename) throws IOException {
-        Path path = Paths.get(dataPath);
+    public class WortSL extends IOException {
+        private WortTrainer trainer;
+        private String filename = "WortTrainer.txt";
+        private String[] liste;
 
-        if (Files.exists(path)) {
-            try (PrintWriter outputStream = new PrintWriter(filename)) {
-                outputStream.println(trainer.toString());
-            }
-        } else {
-            System.out.println("DataPath is incorrect!");
+        public WortSL(WortTrainer trainer) {
+            this.trainer = trainer;
+            liste = trainer.toString().split("\n");
         }
-    }
-    public void speichern() throws IOException {
-        speichern("Downloads", "Worttrainer.txt");
-    }
 
-    public void laden(String dataPath, String filename) throws IOException{
-        Path path = Paths.get(dataPath);
-
-        if (Files.exists(path)) {
-            try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(filename)))) {
-                while (scanner.hasNext()) {
+        public void speichern(String filename) throws IOException {
+            try (PrintWriter ops = new PrintWriter(filename)) {
+                for (String e : liste) {
+                    ops.println(e);
                 }
             }
-        } else {
-            System.out.println("DataPath is incorrect!");
+        }
+
+        public void speichern() throws IOException {
+            speichern(this.filename);
+        }
+
+        public void laden(String dataPath, String filename) throws IOException {
+            Path path = Paths.get(dataPath);
+
+            if (Files.exists(path)) {
+                try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(filename)))) {
+                    while (scanner.hasNext()) {
+                    }
+                }
+            } else {
+                System.out.println("DataPath is incorrect!");
+            }
         }
     }
-    public void laden(){
-
-    }
-}
