670941a9498822d856097bd9f237496ebf09c092
Maged Negm <mnegm@student.tgm.at.ac>
2023-11-18 18:03:50 +0100

    fehlermeldungen zur speichern Methode hinzugefügt

--- src/mnegm/WortSL.java
+++ src/mnegm/WortSL.java
@@ -4,38 +4,36 @@ import java.io.*;
 import java.nio.file.*;
 import java.util.Scanner;
 
-    public class WortSL extends IOException {
-        private WortTrainer trainer;
-        private String filename = "WortTrainer.txt";
-        private String[] liste;
+public class WortSL extends IOException {
+    private WortTrainer trainer;
+    private String filename = "WortTrainer.txt";
+    private String[] liste;
 
-        public WortSL(WortTrainer trainer) {
-            this.trainer = trainer;
-            liste = trainer.toString().split("\n");
+    public WortSL(WortTrainer trainer) {
+        this.trainer = trainer;
+        liste = trainer.toString().split("\n");
+    }
+
+    public void speichern(String filename) throws IOException, IllegalArgumentException {
+        File file = new File(filename);
+        if(!file.exists()){
+            throw new FileNotFoundException("Diese Datei existiert nicht!");
         }
-
-        public void speichern(String filename) throws IOException {
-            try (PrintWriter ops = new PrintWriter(filename)) {
-                for (String e : liste) {
-                    ops.println(e);
-                }
-            }
+        if(!file.canWrite()){
+            throw new IllegalArgumentException("In diese Datei kann nicht geschrieben werden! - Du hast nicht die Berechtigung!");
         }
-
-        public void speichern() throws IOException {
-            speichern(this.filename);
-        }
-
-        public void laden(String dataPath, String filename) throws IOException {
-            Path path = Paths.get(dataPath);
-
-            if (Files.exists(path)) {
-                try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(filename)))) {
-                    while (scanner.hasNext()) {
-                    }
-                }
-            } else {
-                System.out.println("DataPath is incorrect!");
+        try (PrintWriter ops = new PrintWriter(filename)) {
+            for (String e : liste) {
+                ops.println(e);
             }
         }
     }
+
+    public void speichern() throws IOException {
+        speichern(this.filename);
+    }
+
+    public WortTrainer laden(String filename) {
+
+    }
+}
