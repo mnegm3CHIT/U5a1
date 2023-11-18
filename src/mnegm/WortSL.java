7d1309b51e8beda7dc38222cc363688f42c30e1a
Maged Negm <mnegm@student.tgm.at.ac>
2023-11-18 21:42:49 +0100

    Domkumentationskommentare

--- src/mnegm/WortSL.java
+++ src/mnegm/WortSL.java
@@ -1,25 +1,44 @@
 package mnegm;
 
 import java.io.*;
-import java.nio.file.*;
 import java.util.Scanner;
+
+/**
+ * Die Klasse WortSL erweitert die IOException und dient der Speicherung und dem Laden
+ * von Daten eines WortTrainers in eine Datei.
+ *
+ * @author Maged Negm
+ * @version 2023-10-20
+ */
 
 public class WortSL extends IOException {
     private WortTrainer trainer;
-    private final String FILENAME = "WortTrainer.txt";
+    private final String FILENAME = "C:\\Users\\Maged\\OneDrive\\Schule\\Schuljahr 2022 2024\\SEW\\U5a1\\WortTrainer.txt";
     private String[] liste;
 
+    /**
+     * Konstruktor der Klasse WortSL.
+     *
+     * @param trainer Der übergebene WortTrainer, dessen Daten gespeichert werden sollen.
+     */
     public WortSL(WortTrainer trainer) {
         this.trainer = trainer;
         liste = trainer.toString().split("\n");
     }
 
+    /**
+     * Speichert die Daten des WortTrainers in eine Datei mit dem angegebenen Dateinamen.
+     *
+     * @param filename Der Dateiname, unter dem die Daten gespeichert werden sollen.
+     * @throws IOException Wenn ein Fehler beim Schreiben in die Datei auftritt.
+     * @throws IllegalArgumentException Wenn die Datei nicht existiert oder nicht beschreibbar ist.
+     */
     public void speichern(String filename) throws IOException, IllegalArgumentException {
         File file = new File(filename);
-        if(!file.exists()){
+        if (!file.exists()) {
             throw new FileNotFoundException("Diese Datei existiert nicht!");
         }
-        if(!file.canWrite()){
+        if (!file.canWrite()) {
             throw new IllegalArgumentException("In diese Datei kann nicht geschrieben werden! - Du hast nicht die Berechtigung!");
         }
         try (PrintWriter ops = new PrintWriter(filename)) {
@@ -29,16 +48,29 @@ public class WortSL extends IOException {
         }
     }
 
+    /**
+     * Speichert die Daten des WortTrainers in die vordefinierte Datei.
+     *
+     * @throws IOException Wenn ein Fehler beim Schreiben in die Datei auftritt.
+     */
     public void speichern() throws IOException {
         speichern(FILENAME);
     }
 
+    /**
+     * Lädt die Daten eines WortTrainers aus einer Datei mit dem angegebenen Dateinamen.
+     *
+     * @param filename Der Dateiname, aus dem die Daten geladen werden sollen.
+     * @return Der geladene WortTrainer.
+     * @throws IOException Wenn ein Fehler beim Lesen aus der Datei auftritt.
+     * @throws IllegalArgumentException Wenn die Datei nicht existiert oder nicht lesbar ist.
+     */
     public WortTrainer laden(String filename) throws IOException, IllegalArgumentException {
         File file = new File(filename);
-        if(!file.exists()){
+        if (!file.exists()) {
             throw new FileNotFoundException("Diese Datei existiert nicht!");
         }
-        if(!file.canRead()){
+        if (!file.canRead()) {
             throw new IllegalArgumentException("Diese Datei kann nicht gelesen werden! - Du hast nicht die Berechtigung!");
         }
         String text = "";
@@ -53,9 +85,9 @@ public class WortSL extends IOException {
         String[] s = part[1].split("[a-zA-Z\"\"\n ()]+");
 
         WortListe wl = new WortListe(1);
-        for(String ce : e) {
+        for (String ce : e) {
             String[] teil = ce.split(";");
-            if(teil[0].equals("<leer>"))
+            if (teil[0].equals("<leer>"))
                 wl.addWord(null);
             else
                 wl.addWord(teil[0], teil[1]);
@@ -63,9 +95,16 @@ public class WortSL extends IOException {
 
         WortStatistik ws = new WortStatistik(Integer.parseInt(s[2]), Integer.parseInt(s[3]));
 
-        return  new WortTrainer(wl, null, ws);
+        return new WortTrainer(wl, null, ws);
     }
 
+    /**
+     * Lädt die Daten eines WortTrainers aus der vordefinierten Datei.
+     *
+     * @return Der geladene WortTrainer.
+     * @throws IOException Wenn ein Fehler beim Lesen aus der Datei auftritt.
+     * @throws IllegalArgumentException Wenn die Datei nicht existiert oder nicht lesbar ist.
+     */
     public WortTrainer laden() throws IOException, IllegalArgumentException {
         return this.laden(FILENAME);
     }
