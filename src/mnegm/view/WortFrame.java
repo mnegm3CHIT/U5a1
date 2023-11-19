86249cbb1124d96e0c8686d0485c1e4072e6153f
Maged Negm <mnegm@student.tgm.at.ac>
2023-11-19 13:01:09 +0100

    Konstruktor der Frame-klasse erstellt

--- src/mnegm/view/WortFrame.java
+++ src/mnegm/view/WortFrame.java
@@ -3,4 +3,14 @@ package mnegm.view;
 import javax.swing.*;
 
 public class WortFrame extends JFrame {
+    public WortFrame(JPanel WortLayout) {
+        this.setTitle("Worttrainer_mnegm3CHIT");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.add(WortLayout);
+        this.setVisible(true);
+        this.setResizable(true);
+        this.pack();
+        this.setLocationRelativeTo(null);
+    }
 }
